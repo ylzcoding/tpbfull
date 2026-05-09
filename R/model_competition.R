eb8a8e24be483110d1cb21a4dbc0608b3618007d
Zhu, Yilin <yilinz10@illinois.edu>
2026-05-08 23:53:25 -0400

    change candidates

--- R/model_competition.R
+++ R/model_competition.R
@@ -448,7 +448,7 @@ run_model_competition <- function(X, y,
                                   candidates = list(
                                     hs = list(a = 0.5, b = 0.5),
                                     normal_gamma = list(a = 0.5, b = sqrt(1000.0)),
-                                    ridge = list(a = sqrt(1000.0), b = sqrt(1000.0))
+                                    student_t = list(a = sqrt(1000.0), b = 1.0)
                                   ),
                                   pre_opt_burnin = 200,
                                   pre_opt_samples = 200,
