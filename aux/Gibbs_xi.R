fb3d40a7cf2f0795a85b7b07ffe299e510f96a79
Zhu, Yilin <yilinz10@illinois.edu>
2026-05-12 23:32:18 -0400

    roll back to nu/lambda parameterization

--- aux/Gibbs_xi.R
+++
@@ -1,5 +0,0 @@
-#' @param p ncol(X)
-#' @param a hyper-parameter, real number
-#' @param nu p*1 coefficient vector
-#' @export
-Gibbs_xi = function(a, p, nu){return(rgamma(p, shape = a, rate = 1/nu))}
